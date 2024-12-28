1150f4aae62478c6cf537a60a151327f78df305f
Mike Berrio <mikeberrio1@gmail.com>
2024-12-28 03:08:33 +0000

    finished the first 19 exe

--- exercises/19-Bottles-Of-Milk/app.py
+++ exercises/19-Bottles-Of-Milk/app.py
@@ -1,15 +1,15 @@
 # ✅↓ Write your code here ↓✅
 def number_of_bottles(): 
-    numberOfBottles = 5 
-    for i in range(numberOfBottles, 0, -1): 
-        bottle_word = "bottle" if number_of_bottles == 1 else "bottles"
-        print(f"{numberOfBottles} {bottle_word} of milk on the wall, {numberOfBottles} {bottle_word} of milk.") 
-        numberOfBottles -= 1 
-        bottle_word = "bottle" if number_of_bottles == 1 else "bottles"
-        if numberOfBottles == 0:
-            print ("No more bottles of milk on the wall, no more bottles of milk.")
-            print ("Go to the store and buy some more, 99 bottles of milk on the wall.")
+    numberOfBottles = 99 
+    for i in range(numberOfBottles, 0, -1):     
+        bottle_word = "bottle" if numberOfBottles == 1 else "bottles"
+        
+        if numberOfBottles == 1:
+            print (f"{numberOfBottles} {bottle_word} of milk on the wall, {numberOfBottles} {bottle_word} of milk. Take one down and pass it around, no more bottles of milk on the wall.")
+            print ("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.")
         else:
-            print(f"Take one down and pass it around, {numberOfBottles} {bottle_word} of milk on the wall.")
+            single_word = "bottle" if numberOfBottles - 1 == 1 else "bottles"
+            print(f"{numberOfBottles} {bottle_word} of milk on the wall, {numberOfBottles} {bottle_word} of milk. Take one down and pass it around, {numberOfBottles - 1} {single_word} of milk on the wall.") 
 
+        numberOfBottles -= 1
 number_of_bottles()
